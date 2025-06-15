b9d96efd20bc35f2058ab96a8bce9f475089067d
asadnaved9 <asadnaved4@gmail.com>
2025-06-15 12:16:32 +0530

    Page 1 done with the animations

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-
-var tl = gsap.timeline();
+function loadingAnimation(){
+    var tl = gsap.timeline();
 tl.from(".line h1",{
     y:150,
     stagger:0.25,
     duration:0.6,
     delay:0.5,
 });
-
 tl.from("#line1-part1",{
     opacity:0,
     onStart:function(){
@@ -26,7 +25,6 @@ tl.to(".line h2",{
     animationName:"anime",
     opacity:1
 })
-
 tl.to("#loader",{
     opacity:0,
     duration:0.2,
@@ -40,5 +38,28 @@ tl.from("#page1",{
     ease:Power4,
 });
 tl.to("#loader",{
-    display:"none"
+    display:"none",
+});
+tl.from("#nav",{
+    opacity:0,
 })
+tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
+    y:120,
+    stagger:0.2,
+});
+}
+
+
+function cursorAnimation(){
+    document.addEventListener("mousemove",function(dets){
+        gsap.to("#crsr",{
+            left:dets.x,
+            top:dets.y   
+        });
+    });
+    // For making the magnet effect on the element
+    Shery.makeMagnet("#nav-part2 h4" /* Element to target.*/);
+}
+
+loadingAnimation();
+cursorAnimation();
